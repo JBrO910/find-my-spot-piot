4a151ba7bb8b970c717a9f6f519b037ca07ec84b
Jonathan Brocksieper <58165762+JBrO910@users.noreply.github.com>
2022-10-03 17:05:05 +0200

    Added WiFi and MQTT on Lopy

---
+++ spot_checker/src/mqtt.py
@@ -0,0 +1,15 @@
+from umqttsimple import MQTTClient
+import machine
+import time
+
+def connect_mqtt(client_id, mqtt_server):
+    client = MQTTClient(client_id, mqtt_server)
+    client.connect()
+    print('Connected to %s MQTT broker' % (mqtt_server))
+    return client
+
+
+def restart_and_reconnect():
+    print('Failed to connect to MQTT broker. Reconnecting...')
+    time.sleep(10)
+    machine.reset()

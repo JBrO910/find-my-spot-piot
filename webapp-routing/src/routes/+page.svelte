b969d878ec6efee5dbab26accca80037898cbff9
Joshua Gawenda <joshua.gawenda@gmail.com>
2022-10-16 17:48:35 +0200

    Add webapp with routing

---
+++ webapp-routing/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang='ts'>
+  import GarageListEntry from '$lib/components/GarageListEntry.svelte'
+  import AppBarLayout from '$lib/layout/AppBarLayout.svelte'
+  import type { GarageOverview } from '../lib/types'
+
+  export let data: { garages: Array<GarageOverview> }
+</script>
+
+<AppBarLayout>
+    <p slot='content' class='text-gray-50 text-md italic'>Overview</p>
+
+    <main class='flex gap-4 flex-col py-4 px-4'>
+        {#each data?.garages ?? [] as garage}
+            <GarageListEntry garage={garage} />
+        {/each}
+    </main>
+</AppBarLayout>

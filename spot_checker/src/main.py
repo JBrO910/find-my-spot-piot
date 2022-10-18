c2c199237d64cd4755ab88e7ce7f622d30328491
Jonathan Brocksieper <58165762+JBrO910@users.noreply.github.com>
2022-10-18 16:25:54 +0200

    Spot registering

--- spot_checker/src/main.py
+++ spot_checker/src/main.py
@@ -1,21 +1,11 @@
 import pycom
 from time import sleep
-from hcsr04 import HCSR04
-from machine import Pin
 import machine
-from ENV import TRIGGER_DISTANCE
 import ubinascii
 import mqtt
-import json
 from measure_behavior import SpotBehavior
 
 pycom.heartbeat(False)
-
-# Sensor definitions
-spot_1 = SpotBehavior('P23', 'P22', 'P21', 'P20')
-spot_2 = SpotBehavior('P9', 'P10', 'P11', 'P12')
-spot_3 = SpotBehavior('P3', 'P4', 'P19', 'P8')
-
 
 # MQTT definition
 mqtt_server = '192.168.204.111'
@@ -28,6 +18,14 @@ try:
     client = mqtt.connect_mqtt(client_id, mqtt_server)
 except OSError as e:
     mqtt.restart_and_reconnect()
+
+# Sensor definitions
+spot_1 = SpotBehavior('001', 'HY 01','P23', 'P22', 'P21', 'P20', client)
+spot_2 = SpotBehavior('002', 'HY 02','P9', 'P10', 'P11', 'P12', client)
+spot_3 = SpotBehavior('003', 'HY 03', 'P3', 'P4', 'P19', 'P8', client)
+spot_1.register()
+spot_2.register()
+spot_3.register()
 
 while True:
     try:

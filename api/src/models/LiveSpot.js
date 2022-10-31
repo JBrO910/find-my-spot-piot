6ac0a4ef69b55adcf7534a30f4f638017f7d7418
Joshua Gawenda <joshua.gawenda@gmail.com>
2022-10-31 16:37:56 +0100

    Remove redundant data

--- api/src/models/LiveSpot.js
+++ api/src/models/LiveSpot.js
@@ -3,20 +3,17 @@ export const MAX_KEEP_ALIVE_GAP = 3 * KEEP_ALIVE_INTERVAL
 
 export default class LiveSpot {
   id
-  garage
   status
   statusChangedAt
   lastKeepAlive
 
   constructor(
-    garage,
     status,
     id,
     statusChangedAt = new Date().getTime(),
     lastKeepAlive = new Date().getTime(),
   ) {
     this.id = id
-    this.garage = garage
     this.status = status
     this.statusChangedAt = statusChangedAt
     this.lastKeepAlive = lastKeepAlive
@@ -25,21 +22,19 @@ export default class LiveSpot {
   get isFree() {
     const isStatusFree = this.status > 0
     const isAlive =
-      new Date().getTime() - this.lastKeepAlive > MAX_KEEP_ALIVE_GAP
+      new Date().getTime() - this.lastKeepAlive < MAX_KEEP_ALIVE_GAP
     return isStatusFree && isAlive
   }
 
   get serialised() {
     return {
-      garage: this.garage,
       status: this.status,
-      isFree: this.isFree,
       statusChangedAt: this.statusChangedAt,
       lastKeepAlive: this.lastKeepAlive,
     }
   }
 
-  static fromSerialized({ garage, status, id, statusChangedAt, lastKeepAlive }) {
-    return new LiveSpot(garage, status, id, statusChangedAt, lastKeepAlive)
+  static fromSerialized({ status, id, statusChangedAt, lastKeepAlive }) {
+    return new LiveSpot(status, id, statusChangedAt, lastKeepAlive)
   }
 }

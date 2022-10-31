0b0f5a4a1963d49c0ba835508604f65966f0556e
Joshua Gawenda <joshua.gawenda@gmail.com>
2022-10-31 16:40:13 +0100

    Add error display

--- webapp-routing/src/lib/layout/AppBarLayout.svelte
+++ webapp-routing/src/lib/layout/AppBarLayout.svelte
@@ -1,16 +1,27 @@
 <script>
-    import SearchField from '$lib/components/Searchfield.svelte'
+    import Banner from '$lib/components/Banner.svelte'
     import TopBar from '$lib/components/TopBar.svelte'
+
+    export let error
 </script>
 
 <div class='h-screen flex flex-col bg-gray-100'>
-    <TopBar title='FindMySpot'>
-        <slot
-                name='content'
-                slot='content'
-        >
-            <SearchField />
-        </slot>
-    </TopBar>
+  <TopBar
+    other='"'
+    title='FindMySpot'
+  >
+    <slot
+      name='content'
+      slot='content'
+    >
+    </slot>
+  </TopBar>
+
+  {#if error}
+    <Banner type='error' title={error.message} description={error.description} />
+  {/if}
+
+  <div class='h-screen overflow-y-auto'>
     <slot />
+  </div>
 </div>

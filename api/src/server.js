886a88245958e1bdaf6666f904454b0e3a423750
Joshua Gawenda <joshua.gawenda@gmail.com>
2022-10-31 16:38:59 +0100

    Logging of requests

--- api/src/server.js
+++ api/src/server.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use((req, res, next) => {
-  Log.tag("REQUEST").trace(req.originalUrl)
+  Log.trace(req.method, req.originalUrl)
   next()
 })
 

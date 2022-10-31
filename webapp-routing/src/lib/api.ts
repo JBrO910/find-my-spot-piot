d6d413742223f1b0f29ddb92a714b613cce67818
Joshua Gawenda <joshua.gawenda@gmail.com>
2022-10-31 16:40:30 +0100

    Add error display

--- webapp-routing/src/lib/api.ts
+++ webapp-routing/src/lib/api.ts
@@ -1,19 +1,61 @@
 // @ts-ignore
 import { PUBLIC_API_URL } from '$env/static/public'
+import type { AxiosError, AxiosResponse } from 'axios'
 import axios from 'axios'
-import type { Garage, GarageOverview } from './types'
+import type { ApiError, CombinedSpot, Garage, GarageOverview, WithError } from './types'
 
 const instance = axios.create({
   baseURL: PUBLIC_API_URL,
   withCredentials: false,
 })
 
-export const getGarageOverview = async (): Promise<Array<GarageOverview>> => {
-  return instance.get<Array<GarageOverview>>('garage/overview')
-    .then(res => res.data)
+const handleError = (axiosError: AxiosError): WithError<any> => {
+  let error = {
+    message: axiosError.message,
+    code: axiosError.code,
+  } as ApiError
+
+  if (!axiosError.response) {
+    error.message = 'API is not reachable'
+    error.description = 'Please check your internet connection'
+  }
+
+  return {
+    data: undefined,
+    error,
+  }
 }
 
-export const getGarage = async (id: string): Promise<Garage> => {
-  return instance.get<GarageOverview>(`garage/${id}`)
-    .then(res => res.data)
+const handleResponse = <T>({ data }: AxiosResponse<T>) => {
+  return {
+    data,
+    error: undefined,
+  }
+}
+
+// TODO Error message
+
+export const getGarageOverview = async (): Promise<WithError<Array<GarageOverview>>> => {
+  return await instance.get<Array<GarageOverview>>('garage/overview')
+    .then(handleResponse)
+    .catch(handleError)
+}
+
+export const getGarage = async (id: string): Promise<WithError<Garage | undefined>> => {
+  return await instance.get<Garage>(`garage/${ id }`)
+    .then(handleResponse)
+    .catch(handleError)
+}
+
+export const getGarageSpots = async (id?: string): Promise<WithError<Record<string, CombinedSpot> | undefined>> => {
+  if (!id) {
+    return {
+      data: undefined,
+      error: { message: 'Invalid id was provided' },
+    }
+  }
+
+  return await instance.get<Record<string, CombinedSpot>>(`garage/${ id }/spots`)
+    .then(handleResponse)
+    .catch(handleError)
 }
--- webapp-routing/src/lib/types.ts
+++ webapp-routing/src/lib/types.ts
@@ -1,9 +1,18 @@
+export interface ApiError {
+  message?: string
+  description?: string
+  code?: string
+}
+
+export type WithError<T> = { data?: T, error?: ApiError }
+
 export interface Garage {
   id: string
 
   name: string
   address: string
   phoneNumber: string
+  levelDescription: Array<{x: Number, y: Number}>
 }
 
 export interface GarageOverview extends Garage{
@@ -15,7 +24,18 @@ export interface Spot {
   id: string
 
   garage: string
+  localIdentifier?: string
+  x: number
+  y: number
+  z: number
+}
+
+export interface LiveSpot {
+  id: string
+
   status: 0 | 1
   statusChangedAt: number
   lastKeepAlive: number
 }
+
+export type CombinedSpot = Spot & Partial<LiveSpot>

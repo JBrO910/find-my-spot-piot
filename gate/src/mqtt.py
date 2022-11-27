8e920d30aaf39e287d60d284122ece4b256dd864
Jonathan Brocksieper <58165762+JBrO910@users.noreply.github.com>
2022-11-27 16:24:56 +0100

    Added gate codebase

---
+++ gate/src/mqtt.py
@@ -0,0 +1,16 @@
+from umqttsimple import MQTTClient, MQTTException
+import machine
+import time
+
+
+def connect_mqtt(client_id, mqtt_server):
+    client = MQTTClient(client_id, mqtt_server)
+    client.connect()
+    print('Connected to %s MQTT broker' % (mqtt_server))
+    return client
+
+
+def restart_and_reconnect():
+    print('Failed to connect to MQTT broker. Reconnecting...')
+    time.sleep(10)
+    machine.reset()

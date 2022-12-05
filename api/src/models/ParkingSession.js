86ee63865fe8787119dbe071fd2ed95049a6b58f
Joshua Gawenda <joshua.gawenda@gmail.com>
2022-12-05 16:36:47 +0100

    Fix fromSerialized

--- api/src/models/ParkingSession.js
+++ api/src/models/ParkingSession.js
@@ -1,61 +1,83 @@
 export default class ParkingSession {
-    id
-    userId
-    garageId
+  id
+  userId
+  garageId
 
-    startTime
-    endTime
+  startTime
+  endTime
 
-    hourlyRate
-    maxRate
-    totalCost
+  hourlyRate
+  maxRate
+  totalCost
 
-    status
+  status
 
-    constructor(userId, garageId, hourlyRate, maxRate, startTime, endTime, status, totalCost, id) {
-        this.userId = userId
-        this.garageId = garageId
-        this.startTime = startTime ?? new Date().getTime()
-        this.endTime = endTime
-        this.hourlyRate = hourlyRate
-        this.maxRate = maxRate
-        this.totalCost = totalCost
-        this.status = status ?? !this.endTime ? "open" : "unpaid"
-        this.id = id
-    }
+  constructor(
+    userId,
+    garageId,
+    hourlyRate,
+    maxRate,
+    startTime,
+    endTime,
+    status,
+    totalCost,
+    id,
+  ) {
+    this.userId = userId
+    this.garageId = garageId
+    this.startTime = startTime ?? new Date().getTime()
+    this.endTime = endTime
+    this.hourlyRate = hourlyRate
+    this.maxRate = maxRate
+    this.totalCost = totalCost
+    this.status = status ?? !this.endTime ? 'open' : 'unpaid'
+    this.id = id
+  }
 
-    calculateTotalCost() {
-        const timeElapsed = this.endTime - this.startTime
-        const hoursElapsed = timeElapsed / 1000 / 60 / 60
-        const totalCost = hoursElapsed * this.hourlyRate
-        return totalCost > this.maxRate ? this.maxRate : totalCost
-    }
+  calculateTotalCost() {
+    const timeElapsed = this.endTime - this.startTime
+    const hoursElapsed = timeElapsed / 1000 / 60 / 60
+    const totalCost = hoursElapsed * this.hourlyRate
+    return totalCost > this.maxRate ? this.maxRate : totalCost
+  }
 
-    closeSession() {
-        this.endTime = new Date().getTime()
-        this.totalCost = this.calculateTotalCost()
-        this.status = "unpaid"
-    }
+  closeSession() {
+    this.endTime = new Date().getTime()
+    this.totalCost = this.calculateTotalCost()
+    this.status = 'unpaid'
+  }
 
-    pay() {
-        this.status = "closed"
-    }
+  pay() {
+    this.status = 'closed'
+  }
 
-    get serialised() {
-        return Object.fromEntries(Object.entries(this).filter(([,value]) => value !== undefined))
-    }
+  get serialised() {
+    return Object.fromEntries(
+      Object.entries(this).filter(([, value]) => value !== undefined),
+    )
+  }
 
-    static fromSerialized({endTime, hourlyRate, status, maxRate, startTime, totalCost, userId, garageId, id}) {
-        return new ParkingSession(
-            userId,
-            garageId,
-            startTime,
-            endTime,
-            hourlyRate,
-            maxRate,
-            totalCost,
-            status,
-            id,
-        )
-    }
+  static fromSerialized({
+    userId,
+    garageId,
+    hourlyRate,
+    maxRate,
+    startTime,
+    endTime,
+    status,
+    totalCost,
+    id,
+  }) {
+    return new ParkingSession(
+      userId,
+      garageId,
+      hourlyRate,
+      maxRate,
+      startTime,
+      endTime,
+      status,
+      totalCost,
+      id,
+    )
+  }
 }

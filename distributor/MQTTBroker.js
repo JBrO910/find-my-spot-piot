7c74f501f05222d246d3aa7a03af8430585236f7
Joshua Gawenda <joshua.gawenda@gmail.com>
2023-01-03 17:21:15 +0100

    Debug Distributor

--- distributor/MQTTBroker.js
+++ distributor/MQTTBroker.js
@@ -1,6 +1,6 @@
-import axios from "axios";
-import mqtt from "mqtt";
-import { Log } from "./logger";
+import axios from 'axios'
+import mqtt from 'mqtt'
+import { Log } from './logger'
 import {
     emitKeepAliveSpot,
     emitLoadSpots,
@@ -9,7 +9,8 @@ import {
     emitUpdateSpot,
     listenToBlinkMaintain,
     listenToLoadSpots,
-    listenToMeasureMaintain, listenToOpenGateMaintain,
+    listenToMeasureMaintain,
+    listenToOpenGateMaintain,
     listenToReadCard,
     listenToRegisterMaintain,
     listenToTurnOffMaintain,
@@ -24,7 +25,8 @@ import {
     GATE_SEND_UID_RESPONSE,
     KEEP_ALIVE,
     MEASURE,
-    MEASURE_RESPONSE, OPEN_GATE,
+    MEASURE_RESPONSE,
+    OPEN_GATE,
     RECEIVE_ID,
     REQUEST_ID,
     REQUEST_ID_RESPONSE,
@@ -32,26 +34,30 @@ import {
     TURN_ON,
     UPDATE_SPOT,
 } from './topics'
-import { sleep } from "./utils";
+import { sleep } from './utils'
 
 let instance = axios.create({
     baseURL:
         process.env.SOCKET_SERVER_ADDRESS +
-        "/parkingSession/" +
+        '/parkingSession/' +
         process.env.GARAGE_ID +
-        "/toggleSession",
+        '/toggleSession',
     withCredentials: false,
-});
+})
 
 // ? Sleep time defaults to 10 seconds
 export default function setupMQTTBroker(registerSleepTime = 1000 * 10) {
-    const options = process.env.ENVIRONMENT === "production" ? {
+    const options = process.env.ENVIRONMENT === 'production' ? {
         username: process.env.MQTT_BROKER_USERNAME,
         password: process.env.MQTT_BROKER_PASSWORD,
         clientId: process.env.GARAGE_ID,
         clean: true,
+        hostname: process.env.MQTT_BROKER_ADDRESS,
+        port: 1883,
+        protocol: 'ssl',
     } : {}
-    const mqttClient = mqtt.connect(process.env.MQTT_BROKER_ADDRESS, options);
+    Log.trace('MQTT Broker options', options)
+    const mqttClient = mqtt.connect(process.env.MQTT_BROKER_ADDRESS, options)
 
     let isRegistering = false
     const registeredSpots = []
@@ -62,106 +68,106 @@ export default function setupMQTTBroker(registerSleepTime = 1000 * 10) {
         [KEEP_ALIVE]: mqttParseMessage(emitKeepAliveSpot),
         [CARD_REGISTER_RESPONSE]: mqttParseMessage(emitResultOfReadCard),
         [REQUEST_ID_RESPONSE]: mqttParseMessage(async ({type, data}) => {
-            if (type === "spots") {
-                Log.trace("Register spots", data);
-                registeredSpots.push(...data);
-            } else if (type === "gate") {
-                Log.trace("Register gate", data);
-                registeredGates.push(data);
+            if (type === 'spots') {
+                Log.trace('Register spots', data)
+                registeredSpots.push(...data)
+            } else if (type === 'gate') {
+                Log.trace('Register gate', data)
+                registeredGates.push(data)
             }
         }),
         [GATE_SEND_UID]: mqttParseMessage(({uid, muid}) => {
-            Log.trace("Gate read card with id", uid)
+            Log.trace('Gate read card with id', uid)
             const giveAccess = (access) => () => {
                 const data = {
                     muid,
-                    access
+                    access,
                 }
                 mqttClient.publish(GATE_SEND_UID_RESPONSE, JSON.stringify(data))
             }
-            instance.post("/", {cardID: uid})
+            instance.post('/', {cardID: uid})
                 .then(giveAccess(true))
                 .catch(giveAccess(false))
         }),
-    };
+    }
 
-    mqttClient.on("error", (err) => {
-        Log.error("Connection error in MQTT", err.message);
+    mqttClient.on('error', (err) => {
+        Log.error('Connection error in MQTT', err.message)
     })
 
-    mqttClient.on("connect", () => {
-        Log.info("Connected to MQTT Client")
+    mqttClient.on('connect', () => {
+        Log.info('Connected to MQTT Client')
 
         listenToReadCard(() => {
-            Log.trace("Register card request send")
-            mqttClient.publish(CARD_REGISTER, "{}")
+            Log.trace('Register card request send')
+            mqttClient.publish(CARD_REGISTER, '{}')
         })
 
         listenToLoadSpots(async () => {
             if (isRegistering) {
-                Log.warn("Register process was already started")
+                Log.warn('Register process was already started')
                 return
             }
             isRegistering = true
             registeredSpots.length = 0  // Clear array
             registeredGates.length = 0  // Clear array
 
-            Log.info("Loading Spots requested")
-            mqttClient.publish(REQUEST_ID, "{}")
+            Log.info('Loading Spots requested')
+            mqttClient.publish(REQUEST_ID, '{}')
 
             await sleep(registerSleepTime)
 
             Log.trace(`Register ${ registeredSpots.length } spots`)
             Log.trace(`Register ${ registeredGates.length } gates`)
 
-            emitLoadSpots(registeredSpots, registeredGates);
+            emitLoadSpots(registeredSpots, registeredGates)
             isRegistering = false
         })
 
         listenToBlinkMaintain((id) => {
-            Log.trace("Blink requested for", id)
+            Log.trace('Blink requested for', id)
             mqttClient.publish(BLINK, JSON.stringify({id}))
         })
         listenToMeasureMaintain((id) => {
-            Log.trace("Measure requested for", id)
+            Log.trace('Measure requested for', id)
             mqttClient.publish(MEASURE, JSON.stringify({id}))
         })
         listenToTurnOnMaintain((id) => {
-            Log.trace("Turn on", id)
+            Log.trace('Turn on', id)
             mqttClient.publish(TURN_ON, JSON.stringify({id}))
         })
         listenToTurnOffMaintain((id) => {
-            Log.trace("Turn off", id)
+            Log.trace('Turn off', id)
             mqttClient.publish(TURN_OFF, JSON.stringify({id}))
         })
         listenToRegisterMaintain((data) => {
-            Log.trace("Registered with", data);
-            mqttClient.publish(RECEIVE_ID, JSON.stringify(data));
-        });
+            Log.trace('Registered with', data)
+            mqttClient.publish(RECEIVE_ID, JSON.stringify(data))
+        })
         listenToOpenGateMaintain((id) => {
-            Log.trace("Request open gate for id", id);
-            mqttClient.publish(OPEN_GATE, JSON.stringify({id}));
-        });
+            Log.trace('Request open gate for id', id)
+            mqttClient.publish(OPEN_GATE, JSON.stringify({id}))
+        })
 
         Object.keys(topics)
             .forEach((topic) => {
                 Log.trace(`Listen to topic "${ topic }"`)
                 mqttClient.subscribe(
                     topic,
-                    (err) => err && Log.error(`Topic "${ topic }" failed with`, err)
-                );
-            });
-    });
+                    (err) => err && Log.error(`Topic "${ topic }" failed with`, err),
+                )
+            })
+    })
 
-    mqttClient.on("message", (topic, message) => {
+    mqttClient.on('message', (topic, message) => {
         if (!(topic in topics)) {
-            Log.tag("GotMessage")
+            Log.tag('GotMessage')
                 .warn(`No listener defined for ${ topic }`)
-            return;
+            return
         }
-        Log.tag("GotMessage")
+        Log.tag('GotMessage')
             .trace(topic, JSON.parse(message.toString()))
 
-        topics[topic](message);
-    });
+        topics[topic](message)
+    })
 }
